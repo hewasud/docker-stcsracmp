acb52732162b3fa06a4d6b700e9c589dcb42b5a9
Sudarsha Hewa <sudarsha@photon.lionsden.net>
2018-08-09 00:41:42 -0400

    fix setup problem when updating hosts in the container

--- content/setup.sh
+++ content/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 echo "Setup STCSRACMP Container..."
-sed  -i -e 's/127.0.0.1.*/127.0.0.1 localhost mcssqldev orb-db-d29.statcan.ca/' /etc/hosts
+cat /etc/hosts | sed  's/127.0.0.1.*/127.0.0.1 localhost mcssqldev orb-db-d29.statcan.ca/' > /etc/hosts2
+cd /etc
+cat hosts2 > hosts
+rm -f hosts2
 echo
